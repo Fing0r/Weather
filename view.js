a3e0117cd8d13fa4b50284c2101b1a7fd50fab83
Fin <fantomma91@gmail.com>
2022-03-07 14:22:59 +0700

    событие клика по названиям города в списке

--- view.js
+++ view.js
@@ -6,6 +6,7 @@ export const WEATHER_UI = {
   NOW_TEMP: document.querySelector('.now__temp'),
   NOW_CITY: document.querySelector('.now__city'),
   NOW_IMG: document.querySelector('.now__img'),
+  CITIES: document.querySelectorAll('.cities__item button'),
   ACTIVE_CLASS: 'active',
   API_KEY: 'f660a2fb1e4bad108d6160b7f58c555f',
   API_URL: 'http://api.openweathermap.org/data/2.5/weather', 
