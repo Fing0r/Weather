58f15148638517b212ce77620ba1a901d1d326a9
Fin <fantomma91@gmail.com>
2022-03-12 18:09:34 +0700

    refact

--- main.js
+++ main.js
@@ -1,131 +1,150 @@
 import {
-  UI_ELEMENTS
+  UI_ELEMENTS,
+  DETAILS,
+  NOW,
+  FORECAST,
+  CONFIG,
+  FAVORITES
 } from "./view.js";
 
-UI_ELEMENTS.TAB_BTNS.forEach(tabBtn => {
-  tabBtn.addEventListener('click', function (e) {
-    removeActiveClass(UI_ELEMENTS.TAB_BTNS)
-    removeActiveClass(UI_ELEMENTS.TAB_INFO)
-    changeTab(e)
-  })
-});
+UI_ELEMENTS.TAB_BTNS.forEach(tabBtn => tabBtn.addEventListener('click', changeTab));
+FAVORITES.NAME.forEach(city => city.addEventListener('click', showWeather));
+FAVORITES.DEL.forEach(close => close.addEventListener('click', deleteFavoriteCity));
+UI_ELEMENTS.SEARCH_FORM.addEventListener('submit', showWeather)
 
-UI_ELEMENTS.SEARCH_FORM.addEventListener('submit', function (e) {
-  e.preventDefault();
-  const cityName = UI_ELEMENTS.SEARCH_INPUT.value.trim();
-
-  loadJson(getUrl(cityName))
-    .then(showNowTabInfo)
-    .catch(console.log)
-  UI_ELEMENTS.SEARCH_INPUT.value = ''
-})
-
-UI_ELEMENTS.CITIES.forEach(city => {
-  city.addEventListener('click', function () {
-    createEventCityWeather(city)
-  })
-});
-
-function createEventCityWeather(city) {
-  const cityName = city.textContent;
-
-  loadJson(getUrl(cityName))
-    .then(showNowTabInfo)
-}
-
-// UI_ELEMENTS.FAVORITE_BTN.addEventListener('click', addFavoriteCity);
-
-function checkFavoriteCity(cityName) {
-  const isCityInList = UI_ELEMENTS.FAVORITE_CITIES.some(item => item === cityName);
-
-  if (isCityInList) {
-    UI_ELEMENTS.FAVORITE_BTN.classList.add(UI_ELEMENTS.ACTIVE_CLASS);
-    UI_ELEMENTS.FAVORITE_BTN.addEventListener('click', removeFavoriteCity);
-    UI_ELEMENTS.FAVORITE_BTN.removeEventListener('click', addFavoriteCity);
-  } else {
-    UI_ELEMENTS.FAVORITE_BTN.classList.remove(UI_ELEMENTS.ACTIVE_CLASS);
-    UI_ELEMENTS.FAVORITE_BTN.removeEventListener('click', removeFavoriteCity);
-    UI_ELEMENTS.FAVORITE_BTN.addEventListener('click', addFavoriteCity);
-  }
-}
-
-function showNowTabInfo(params) {
-  const getUrlImg = (img) => `http://openweathermap.org/img/wn/${img}.png`
-
-  checkFavoriteCity(params.name);
-  UI_ELEMENTS.NOW_TEMP.textContent = Math.round(params.main.temp);
-  UI_ELEMENTS.NOW_CITY.textContent = params.name;
-  UI_ELEMENTS.NOW_IMG.src = getUrlImg(params.weather[0].icon);
-}
-
-function addFavoriteCity() {
-  UI_ELEMENTS.FAVORITE_LIST.append(createCityItemElement());
-  UI_ELEMENTS.FAVORITE_BTN.classList.add(UI_ELEMENTS.ACTIVE_CLASS);
-  UI_ELEMENTS.FAVORITE_BTN.removeEventListener('click', addFavoriteCity);
-  UI_ELEMENTS.FAVORITE_BTN.addEventListener('click', removeFavoriteCity);
-  UI_ELEMENTS.FAVORITE_CITIES.push(UI_ELEMENTS.NOW_CITY.textContent);
-}
-
-function removeFavoriteCity() {
-  UI_ELEMENTS.FAVORITE_BTN.classList.remove(UI_ELEMENTS.ACTIVE_CLASS);
-  UI_ELEMENTS.FAVORITE_BTN.addEventListener('click', addFavoriteCity);
-
-  for (const iterator of UI_ELEMENTS.FAVORITE_LIST.children) {
-    const name = iterator.querySelector('.cities__name');
-    const isValis = UI_ELEMENTS.NOW_CITY.textContent.includes(name.textContent);
-    const indexItem = UI_ELEMENTS.FAVORITE_CITIES.findIndex(item => item === name.textContent)
-
-    if (isValis) {
-      iterator.remove();
-      UI_ELEMENTS.FAVORITE_CITIES.splice(indexItem, 1)
-    }
-  }
-}
-
-function createCityItemElement() {
-  const cityItemElement = document.createElement('li');
-  cityItemElement.classList = 'cities__item';
-
-  const cityItemName = document.createElement('button')
-  cityItemName.classList = 'cities__name';
-  cityItemName.textContent = UI_ELEMENTS.NOW_CITY.textContent;
-  cityItemName.type = 'button';
-
-  const cityItemClose = document.createElement('button')
-  cityItemClose.classList = 'cities__close';
-  cityItemClose.textContent = 'X';
-  cityItemClose.type = 'button';
-
-  cityItemElement.append(cityItemName);
-  cityItemElement.append(cityItemClose);
-
-  addEvent(cityItemName, createEventCityWeather)
-  addEvent(cityItemClose, createEventDeletFavoriteCity)
-
-  return cityItemElement;
-}
-
-function addEvent(button, createFunction) {
-  button.addEventListener('click', function () {
-    createFunction(button)
-  })
-}
-
-function createEventDeletFavoriteCity(cityClose) {
-  cityClose.closest('.cities__item').remove()
-  UI_ELEMENTS.FAVORITE_BTN.classList.remove(UI_ELEMENTS.ACTIVE_CLASS)
-  UI_ELEMENTS.FAVORITE_BTN.addEventListener('click', addFavoriteCity)
-
-  const indexItem = UI_ELEMENTS.FAVORITE_CITIES.findIndex(item => item === cityClose.closest('.cities__item').querySelector('.cities__name').textContent)
-  UI_ELEMENTS.FAVORITE_CITIES.splice(indexItem, 1)
-}
 
 function loadJson(url) {
   return fetch(url).then(response => response.json())
 }
 
-function getUrl(city) {
-  return `${UI_ELEMENTS.API_URL}?q=${city}&appid=${UI_ELEMENTS.API_KEY}&units=metric`;
+function getUrl(city, type) {
+  return `${CONFIG.API}${type}?q=${city}&appid=${CONFIG.KEY}&cnt=${CONFIG.NUMBER_FORECASTS}&units=metric`;
+}
+
+function showWeather(e) {
+  e.preventDefault();
+  const cityName = e.currentTarget.textContent.trim() || UI_ELEMENTS.SEARCH_INPUT.value.trim();
+
+  showNowTab(cityName)
+  showForecast(cityName)
+  if (UI_ELEMENTS.SEARCH_INPUT.value.trim()) {
+    UI_ELEMENTS.SEARCH_INPUT.value = ''
+  }
+}
+
+function showNowTab(city) {
+  loadJson(getUrl(city, CONFIG.WEATHER))
+    .then(showTabInfo)
+    .catch(console.log)
+}
+
+function showForecast(city) {
+  loadJson(getUrl(city, CONFIG.FORECAST))
+    .then(arr)
+    .then(result => {
+      FORECAST.LIST.replaceChildren();
+      result.forEach(el => FORECAST.LIST.append(el));
+    })
+}
+
+function checkFavoriteCity(cityName) {
+  const isCityInList = FAVORITES.CITIES.some(item => item === cityName);
+
+  if (isCityInList) {
+    FAVORITES.ADD.classList.add(UI_ELEMENTS.ACTIVE_CLASS);
+    FAVORITES.ADD.addEventListener('click', removeFavoriteCity);
+    FAVORITES.ADD.removeEventListener('click', addFavoriteCity);
+  } else {
+    FAVORITES.ADD.classList.remove(UI_ELEMENTS.ACTIVE_CLASS);
+    FAVORITES.ADD.removeEventListener('click', removeFavoriteCity);
+    FAVORITES.ADD.addEventListener('click', addFavoriteCity);
+  }
+}
+
+function showTabInfo(city) {
+  checkFavoriteCity(city.name);
+  NOW.TEMP.textContent = `${Math.round(city.main.temp)}°`;
+  NOW.CITY.textContent = city.name;
+  NOW.IMG.src = `${CONFIG.IMG}${city.weather[0].icon}@4x.png`
+
+  DETAILS.CITY.textContent = city.name;
+  DETAILS.TEMPERATURE.textContent = `Temperature: ${Math.round(city.main.temp)}°`;
+  DETAILS.FEELS.textContent = `Feels like: ${Math.round(city.main.feels_like)}°`;
+  DETAILS.WEATHER.textContent = `Weather: ${city.weather[0].main}`;
+  DETAILS.SUNRISE.textContent = `Sunrise: ${getTime(city.sys.sunrise)}`;
+  DETAILS.SUNSET.textContent = `Sunset: ${getTime(city.sys.sunset)}`;
+
+  FORECAST.CITY.textContent = city.name;
+}
+
+
+function getDate(param) {
+  return new Date((param) * 1000).toLocaleDateString('en-GB', {
+    month: 'short',
+    day: '2-digit',
+  })
+}
+
+function getTime(param, timezone = 0) {
+  return new Date((param - timezone) * 1000).toLocaleTimeString('en-GB', {
+    hour: 'numeric',
+    minute: 'numeric'
+  })
+}
+
+function addFavoriteCity() {
+  FAVORITES.LIST.append(createCityItem());
+  FAVORITES.ADD.classList.add(UI_ELEMENTS.ACTIVE_CLASS);
+  FAVORITES.ADD.removeEventListener('click', addFavoriteCity);
+  FAVORITES.ADD.addEventListener('click', removeFavoriteCity);
+  FAVORITES.CITIES.push(NOW.CITY.textContent);
+}
+
+function removeFavoriteCity() {
+  FAVORITES.ADD.classList.remove(UI_ELEMENTS.ACTIVE_CLASS);
+  FAVORITES.ADD.addEventListener('click', addFavoriteCity);
+
+  for (const iterator of FAVORITES.LIST.children) {
+    const name = iterator.querySelector('.cities__name');
+    const isValis = NOW.CITY.textContent.includes(name.textContent);
+    const indexItem = FAVORITES.CITIES.findIndex(item => item === name.textContent)
+
+    if (isValis) {
+      iterator.remove();
+      FAVORITES.CITIES.splice(indexItem, 1);
+      return
+    }
+  }
+}
+
+function deleteFavoriteCity(e) {
+  e.currentTarget.closest('.cities__item').remove()
+  FAVORITES.ADD.classList.remove(UI_ELEMENTS.ACTIVE_CLASS)
+  FAVORITES.ADD.addEventListener('click', addFavoriteCity)
+
+  const cityName = e.currentTarget.closest('.cities__item').querySelector('.cities__name')
+  const indexItem = FAVORITES.CITIES.findIndex(item => item === cityName.textContent)
+  FAVORITES.NAME.splice(indexItem, 1)
+}
+
+function addEvent(button, createFunction) {
+  button.addEventListener('click', createFunction)
+}
+
+function createCityItem() {
+  const cityItem = document.createElement('li');
+  cityItem.classList = 'cities__item';
+  cityItem.insertAdjacentHTML("afterbegin", `
+  <button class="cities__name" type="button">${NOW.CITY.textContent}</button>
+  <button class="cities__close" type="button">X</button>`)
+
+  const cityItemName = cityItem.querySelector('.cities__name')
+  const cityItemClose = cityItem.querySelector('.cities__close')
+
+  addEvent(cityItemName, showWeather)
+  addEvent(cityItemClose, deleteFavoriteCity)
+
+  return cityItem;
 }
 
 function removeActiveClass(elems) {
@@ -136,6 +155,36 @@ function changeTab(e) {
   const isDataAttrCurrentTargetBtn = e.currentTarget.dataset.btn
   const isInfoTargetTab = document.querySelector(`[data-item='${isDataAttrCurrentTargetBtn}']`)
 
+  removeActiveClass(UI_ELEMENTS.TAB_BTNS)
+  removeActiveClass(UI_ELEMENTS.TAB_INFO)
+
   e.currentTarget.classList.add(UI_ELEMENTS.ACTIVE_CLASS)
   isInfoTargetTab.classList.add(UI_ELEMENTS.ACTIVE_CLASS)
+}
+
+function createForecastItem(json) {
+  const forecastItem = FORECAST.ITEM.content.firstElementChild.cloneNode(true)
+  forecastItem.querySelector('.forecast__date').textContent = json.date;
+  forecastItem.querySelector('.forecast__time').textContent = json.time;
+  forecastItem.querySelector('.forecast__temp').textContent = `Temperature: ${json.temp}°`;
+  forecastItem.querySelector('.forecast__feels').textContent = `Feels like: ${json.feels}°`;
+  forecastItem.querySelector('.forecast__precipitation').textContent = json.weather;
+  forecastItem.querySelector('.forecast__img').src = `${CONFIG.IMG}${json.icon}@4x.png`;
+
+  return forecastItem;
+}
+
+function arr(json) {
+  return getForecastInfo(json).map(item => createForecastItem(item))
+}
+
+function getForecastInfo(json) {
+  return json.list.map(item => ({
+    date: getDate(item.dt),
+    time: getTime(item.dt),
+    temp: Math.round(item.main.temp),
+    feels: Math.round(item.main.feels_like),
+    weather: item.weather[0].main,
+    icon: item.weather[0].icon,
+  }))
 }
--- view.js
+++ view.js
@@ -3,15 +3,45 @@ export const UI_ELEMENTS = {
   TAB_INFO: document.querySelectorAll('[data-item]'),
   SEARCH_FORM: document.querySelector('.search'),
   SEARCH_INPUT: document.querySelector('.search__input'),
-  NOW_TEMP: document.querySelector('.now__temp'),
-  NOW_CITY: document.querySelector('.now__city'),
-  NOW_IMG: document.querySelector('.now__img'),
-  CITIES: document.querySelectorAll('.cities__name'),
-  CITIES_DEL: document.querySelectorAll('.cities__close'),
   ACTIVE_CLASS: 'active',
-  API_KEY: 'f660a2fb1e4bad108d6160b7f58c555f',
-  API_URL: 'http://api.openweathermap.org/data/2.5/weather',
-  FAVORITE_BTN: document.querySelector('.now__heart'),
-  FAVORITE_LIST: document.querySelector('.cities__list'),
-  FAVORITE_CITIES: [],
+}
+
+export const CONFIG = {
+  KEY: 'f660a2fb1e4bad108d6160b7f58c555f',
+  API: 'http://api.openweathermap.org/data/2.5/',
+  IMG: 'http://openweathermap.org/img/wn/',
+  WEATHER: 'weather',
+  FORECAST: 'forecast',
+  NUMBER_FORECASTS: 6,
+}
+
+export const FAVORITES = {
+  LIST: document.querySelector('.cities__list'),
+  NAME: document.querySelectorAll('.cities__name'),
+  DEL: document.querySelectorAll('.cities__close'),
+  ADD: document.querySelector('.now__heart'),
+  CITIES: [],
+}
+
+
+export const NOW = {
+  TEMP: document.querySelector('.now__temp'),
+  CITY: document.querySelector('.now__city'),
+  IMG: document.querySelector('.now__img'),
+}
+
+export const DETAILS = {
+  CITY: document.querySelector('.details__city'),
+  INFO: document.querySelector('.details__info'),
+  TEMPERATURE: document.querySelector('.details__temperature'),
+  FEELS: document.querySelector('.details__feels'),
+  WEATHER: document.querySelector('.details__weather'),
+  SUNRISE: document.querySelector('.details__sunrise'),
+  SUNSET: document.querySelector('.details__sunset'),
+}
+
+export const FORECAST = {
+  CITY: document.querySelector('.forecast__city'),
+  LIST: document.querySelector('.forecast__list'),
+  ITEM: document.querySelector('#forecast-item'),
 }
